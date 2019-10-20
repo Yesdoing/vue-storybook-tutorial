0177c41380a621aa3daae90dbf9d06b639a852f1
Jane Lee <Yesdoing13@gmail.com>
2019-10-20 22:54:26 +0900

    refactor: seperate Presentation and Container in TaskList Component

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,7 +2,6 @@ import { configure } from '@storybook/vue';
 
 import '../src/style/index.css';
 
-
 const req = require.context('../src', true, /\.stories.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
-  
+    <task-list />
   </div>
 </template>
 
 <script>
+import store from './store.js';
+import TaskList from './components/TaskList.vue';
+import './style/index.css';
+
 export default {
   name: "app",
+  store,
   components: {
+    TaskList
   }
 };
 </script>
